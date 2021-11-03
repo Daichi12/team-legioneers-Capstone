e9dbd85a452894268827d09ccc323e7a5e9a351b
Daichi12 <hekami10@hotmail.com>
2021-11-03 15:21:45 -0800

    Basic React app

---
+++ porticos-react/src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h2> Hello zhao</h2>
+            </div>
+        )
+
+    }
+}
+
+export default Home
