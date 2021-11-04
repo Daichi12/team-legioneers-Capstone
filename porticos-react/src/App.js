980014b1bae40eae1b43bab97876ae40ad8c010f
ZhaotianWuMelendez <Zhaotian.wu@upr.edu>
2021-11-03 21:04:00 -0400

    JSX convert p2

--- porticos-react/src/App.js
+++ porticos-react/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './Pages/Home';
 
 function App() {
-  document.body.classList.add("home page template-slider color-custom style-default layout-full-width nice-scroll-on button-stroke no-content-padding no-shadows header-split minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menuo-no-borders footer-copy-center mobile-tb-left mobile-side-slide mobile-mini-mr-lc mobile-header-mini mobile-sticky");
+  //document.body.classList.add("home page template-slider color-custom style-default layout-full-width nice-scroll-on button-stroke no-content-padding no-shadows header-split minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menuo-no-borders footer-copy-center mobile-tb-left mobile-side-slide mobile-mini-mr-lc mobile-header-mini mobile-sticky");
   return (
     <Home/>
   );
