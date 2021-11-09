d370b79066673c7b10d3cd5a9f147b48e552dc61
ZhaotianWuMelendez <Zhaotian.wu@upr.edu>
2021-11-08 21:43:47 -0400

    pt2

--- porticos-react/src/Pages/Events.js
+++ porticos-react/src/Pages/Events.js
@@ -1,125 +1,150 @@
 import React, {Component} from 'react';
+import Calendar from 'react-awesome-calendar';
+
+const events = [{
+    id: 1,
+    color: '#000',
+    from: '2021-11-09T10:30:00+00:00',
+    to: '2021-11-09T13:00:00+00:00',
+    title: 'Asamblea de estudiantes'
+}, {
+    id: 2,
+    color: '#1ccb9e',
+    from: '2019-05-01T13:00:00+00:00',
+    to: '2019-05-05T14:00:00+00:00',
+    title: 'This is another event'
+}, {
+    id: 3,
+    color: '#3694DF',
+    from: '2019-05-05T13:00:00+00:00',
+    to: '2019-05-05T20:00:00+00:00',
+    title: 'This is also another event'
+}];
+
 class Events extends React.Component{
 
     render(){
         return(
 
-            <div class="blog color-custom style-default layout-full-width nice-scroll-on button-stroke no-content-padding no-shadows header-split minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menuo-no-borders footer-copy-center mobile-tb-left mobile-side-slide mobile-mini-mr-lc mobile-header-mini mobile-sticky">
-    <div id="Wrapper">
-        <div id="Header_wrapper">
-            <header id="Header">
-                <div class="header_placeholder"></div>
-                <div id="Top_bar">
-                    <div class="container">
-                        <div class="column one">
-                            <div class="top_bar_left clearfix">
-                                <div class="logo">
-                                    <a id="logo" href="/Home" title="Restaurant2 - Theme">
-                                    <img class="logo-main scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
-                                    <img class="logo-sticky scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
-                                    <img class="logo-mobile scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
-                                    <img class="logo-mobile-sticky scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
-                                    </a>
-                                </div>
-                                <div class="menu_wrapper">
-                                    <nav id="menu">
-                                        <ul id="menu-main-menu-left" class="menu menu_left">
-                                            <li>
-                                                <a href="/Home"><span>Home</span></a>
-                                            </li>
-                                            <li>
-                                                <a href="/AboutUs"><span>About us</span></a>
-                                            </li>
-                                            <li class="current_page_parent">
-                                                <a href="/Events"><span>Events</span></a>
-                                            </li>
-                                        </ul>
-                                        <ul id="menu-main-menu-right" class="menu menu_right">
-                                            <li>
-                                                <a href="/Menu"><span>Menu</span></a>
-                                            </li>
-                                            <li>
-                                                <a href="/Contact"><span>Contact</span></a>
-                                            </li>
-                                        </ul>
-                                    </nav><a class="responsive-menu-toggle " href="#"><i class="icon-menu-fine"></i></a>
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-         </header>
-        <footer id="Footer" class="clearfix">
-            <div class="widgets_wrapper" style={{"padding":"120px 0 105px"}}>
-                <div class="container">
+            <Calendar
+                events={events}
+            />
+    //         <div class="blog color-custom style-default layout-full-width nice-scroll-on button-stroke no-content-padding no-shadows header-split minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menuo-no-borders footer-copy-center mobile-tb-left mobile-side-slide mobile-mini-mr-lc mobile-header-mini mobile-sticky">
+    // <div id="Wrapper">
+    //     <div id="Header_wrapper">
+    //         <header id="Header">
+    //             <div class="header_placeholder"></div>
+    //             <div id="Top_bar">
+    //                 <div class="container">
+    //                     <div class="column one">
+    //                         <div class="top_bar_left clearfix">
+    //                             <div class="logo">
+    //                                 <a id="logo" href="/Home" title="Restaurant2 - Theme">
+    //                                 <img class="logo-main scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
+    //                                 <img class="logo-sticky scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
+    //                                 <img class="logo-mobile scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
+    //                                 <img class="logo-mobile-sticky scale-with-grid" src="/assets/content/restaurant2/images/restaurant2.png" alt="restaurant2" />
+    //                                 </a>
+    //                             </div>
+    //                             <div class="menu_wrapper">
+    //                                 <nav id="menu">
+    //                                     <ul id="menu-main-menu-left" class="menu menu_left">
+    //                                         <li>
+    //                                             <a href="/Home"><span>Home</span></a>
+    //                                         </li>
+    //                                         <li>
+    //                                             <a href="/AboutUs"><span>About us</span></a>
+    //                                         </li>
+    //                                         <li class="current_page_parent">
+    //                                             <a href="/Events"><span>Events</span></a>
+    //                                         </li>
+    //                                     </ul>
+    //                                     <ul id="menu-main-menu-right" class="menu menu_right">
+    //                                         <li>
+    //                                             <a href="/Menu"><span>Menu</span></a>
+    //                                         </li>
+    //                                         <li>
+    //                                             <a href="/Contact"><span>Contact</span></a>
+    //                                         </li>
+    //                                     </ul>
+    //                                 </nav><a class="responsive-menu-toggle " href="#"><i class="icon-menu-fine"></i></a>
+    //                                 </div>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //      </header>
+    //     <footer id="Footer" class="clearfix">
+    //         <div class="widgets_wrapper" style={{"padding":"120px 0 105px"}}>
+    //             <div class="container">
 
-                    <div class="column one-third">
-                        <aside id="text-2" class="widget widget_text">
-                            <div class="textwidget">
-                                <div style={{"paddingRight":"20%"}}>
-                                    <h4>Localization</h4>
-                                    <div style={{"width":"30%","height":"1px","background":"#98dace"}}></div>
-                                    <hr class="no_line" style={{"margin":"0 auto 15px"}}/>
-                                    <p>
-                                        <br/> <h5>Calle Dr. Santiago Veve #55
-                                        <br/> San German, Puerto Rico
-                                        <br/> 00683 </h5>
-                                    </p>
-                                </div>
-                            </div>
-                        </aside>
-                    </div>
-                 </div>
+    //                 <div class="column one-third">
+    //                     <aside id="text-2" class="widget widget_text">
+    //                         <div class="textwidget">
+    //                             <div style={{"paddingRight":"20%"}}>
+    //                                 <h4>Localization</h4>
+    //                                 <div style={{"width":"30%","height":"1px","background":"#98dace"}}></div>
+    //                                 <hr class="no_line" style={{"margin":"0 auto 15px"}}/>
+    //                                 <p>
+    //                                     <br/> <h5>Calle Dr. Santiago Veve #55
+    //                                     <br/> San German, Puerto Rico
+    //                                     <br/> 00683 </h5>
+    //                                 </p>
+    //                             </div>
+    //                         </div>
+    //                     </aside>
+    //                 </div>
+    //              </div>
   
 
-                    <div class="column one-third">
-                        <aside id="text-3" class="widget widget_text">
-                            <div class="textwidget">
-                                <div style={{"paddingRight":"20%"}}>
-                                    <h4>Contact</h4>
-                                    <div style={{"width":"30%","height":"1px","background":"#98dace"}}></div>
-                                    <hr class="no_line" style={{"margin":"0 auto 15px"}}/>
-                                    <p style={{"marginBottom":"0px"}}>
-                                        <h5>+1-(787)-978-3555</h5>
-                                    </p>
-                                    <p style={{"wordWrap":"break-word"}}>
-                                        <a href="#"><h5>porticos1606@yahoo.com</h5></a>
-                                    </p>
-                                </div>
-                            </div>
-                        </aside>
-                    </div>
-                    <div class="column one-third">
-                        <aside id="text-4" class="widget widget_text">
-                            <div class="textwidget">
-                                <div style={{"paddingLeft":"20%","textAlign":"right"}}>
-                                    <h4>Working hours</h4>
-                                    <div style={{"width":"30%","display":"block","float":"right","height":"1px","background":"#98dace"}}></div>
-                                    <hr class="no_line" style={{"margin":"0 auto 15px"}}/>
-                                    <p>
-                                        <h5>Our business is open everyday.
-                                        <br /> From 12pm AST. </h5>
-                                    </p>
-                                </div>
-                            </div>
-                        </aside>
-                    </div>
-                 </div>
-        </footer>
+    //                 <div class="column one-third">
+    //                     <aside id="text-3" class="widget widget_text">
+    //                         <div class="textwidget">
+    //                             <div style={{"paddingRight":"20%"}}>
+    //                                 <h4>Contact</h4>
+    //                                 <div style={{"width":"30%","height":"1px","background":"#98dace"}}></div>
+    //                                 <hr class="no_line" style={{"margin":"0 auto 15px"}}/>
+    //                                 <p style={{"marginBottom":"0px"}}>
+    //                                     <h5>+1-(787)-978-3555</h5>
+    //                                 </p>
+    //                                 <p style={{"wordWrap":"break-word"}}>
+    //                                     <a href="#"><h5>porticos1606@yahoo.com</h5></a>
+    //                                 </p>
+    //                             </div>
+    //                         </div>
+    //                     </aside>
+    //                 </div>
+    //                 <div class="column one-third">
+    //                     <aside id="text-4" class="widget widget_text">
+    //                         <div class="textwidget">
+    //                             <div style={{"paddingLeft":"20%","textAlign":"right"}}>
+    //                                 <h4>Working hours</h4>
+    //                                 <div style={{"width":"30%","display":"block","float":"right","height":"1px","background":"#98dace"}}></div>
+    //                                 <hr class="no_line" style={{"margin":"0 auto 15px"}}/>
+    //                                 <p>
+    //                                     <h5>Our business is open everyday.
+    //                                     <br /> From 12pm AST. </h5>
+    //                                 </p>
+    //                             </div>
+    //                         </div>
+    //                     </aside>
+    //                 </div>
+    //              </div>
+    //     </footer>
                
   
-    <div id="Side_slide" class="right dark">
-        <div class="close-wrapper">
-            <a href="#" class="close"><i class="icon-cancel-fine"></i></a>
-        </div>
-        <div class="menu_wrapper"></div>
-    </div>
-    <div id="body_overlay"></div>
+    // <div id="Side_slide" class="right dark">
+    //     <div class="close-wrapper">
+    //         <a href="#" class="close"><i class="icon-cancel-fine"></i></a>
+    //     </div>
+    //     <div class="menu_wrapper"></div>
+    // </div>
+    // <div id="body_overlay"></div>
 
   
-            </div>
-            </div>
-            </div>
+    //         </div>
+    //         </div>
+    //         </div>
 
 
             )
